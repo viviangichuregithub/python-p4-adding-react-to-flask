6466224324d56a48401f19000428225093dd6785
Vivian Gichure <viviangichure@gmail.com>
2025-09-28 19:08:57 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -10,64 +10,43 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
 
 @app.route('/messages', methods=['GET', 'POST'])
 def messages():
     if request.method == 'GET':
-        messages = Message.query.order_by('created_at').all()
+        messages = Message.query.order_by(Message.created_at).all()
+        return jsonify([message.to_dict() for message in messages]), 200
 
-        response = make_response(
-            jsonify([message.to_dict() for message in messages]),
-            200,
-        )
-    
     elif request.method == 'POST':
         data = request.get_json()
-        message = Message(
-            body=data['body'],
-            username=data['username']
-        )
+        if not data.get('body') or not data.get('username'):
+            return jsonify({'error': 'body and username are required'}), 400
 
+        message = Message(body=data['body'], username=data['username'])
         db.session.add(message)
         db.session.commit()
+        return jsonify(message.to_dict()), 201
 
-        response = make_response(
-            jsonify(message.to_dict()),
-            201,
-        )
-
-    return response
 
 @app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    message = Message.query.filter_by(id=id).first()
+    message = Message.query.get_or_404(id)
 
     if request.method == 'PATCH':
         data = request.get_json()
-        for attr in data:
-            setattr(message, attr, data[attr])
-            
-        db.session.add(message)
+        for key, value in data.items():
+            setattr(message, key, value)
         db.session.commit()
-
-        response = make_response(
-            jsonify(message.to_dict()),
-            200,
-        )
+        return jsonify(message.to_dict()), 200
 
     elif request.method == 'DELETE':
         db.session.delete(message)
         db.session.commit()
+        return jsonify({'deleted': True}), 200
 
-        response = make_response(
-            jsonify({'deleted': True}),
-            200,
-        )
-
-    return response
 
 if __name__ == "__main__":
     app.run(port=5555)
